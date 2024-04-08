8902c59cb91b4fd18edda54b8bdf38edcf59ae0e
javierhuertas1 <javierhuertasmedrano385@gmail.com>
2024-04-08 16:27:02 -0500

    Agregando operadores aritmeticos

---
+++ ClaseJavaCertus/src/OperadoresAritmeticos/OperadoresAritmetico.java
@@ -0,0 +1,12 @@
+package OperadoresAritmeticos;
+
+public class OperadoresAritmetico {
+    public static void main(String[] args) {
+        System.out.println("Los operadores aritmeticos son ");
+        System.out.println("+  para sumar");
+        System.out.println("-  para restar");
+        System.out.println("*  para mutplicar");
+        System.out.println("/  para dividir");
+       
+    }
+}
