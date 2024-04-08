e362824ea03ef4ef408cc89a1e89e5c46641209d
javierhuertas1 <javierhuertasmedrano385@gmail.com>
2024-04-08 16:35:51 -0500

    Agregando los operadores aritmeticos

---
+++ OperdoresAritmeticos/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("+ para sumar ");
+        System.out.println("- para restar ");
+        System.out.println("* para multiplicar ");
+        System.out.println("/ para dividir");
+    }
+}
