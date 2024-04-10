e7ed5606f73bf6961125cbdcaba32825193c0877
javierhuertas1 <javierhuertasmedrano385@gmail.com>
2024-04-09 21:25:59 -0500

    Constructores y sobre carga de constructires

--- ObjetoEnJava/src/App.java
+++ ObjetoEnJava/src/App.java
@@ -1,14 +1,35 @@
 public class App {
     public static void main(String[] args) throws Exception {
-
+/* 
         Persona persona1 = new Persona();
 
-        persona1.modificarNombre("Rodrigo");
-        System.out.println("El nombre de la persona es "+persona1.obtenerNombre());
+        persona1.setNombre("Rodrigo");
+        System.out.println("El nombre de la persona es "+persona1.getNombre());
         
 
-        persona1.modificarEdad(24);
-       System.out.println("La edad de la persona es "+persona1.obtenerEdad());
+        persona1.setEdad(24);
+       System.out.println("La edad de la persona es "+persona1.getEdad());
         
+*/
+
+
+
+            Persona persona1 = new Persona("Javier",15);
+            Persona persona2 = new Persona();
+            Persona persona3 = new Persona("Javier",15,784512478,985478965,"Direccion");
+
+            System.out.println("El nombre de la persona es "+ persona1.getNombre());
+            System.out.println("La edad de la persona es "+ persona1.getEdad());
+
+            System.out.println("El nombre de la persona 3 es "+persona3.getNombre());
+            System.out.println("El edad de la persona 3 es "+persona3.getEdad());
+            System.out.println("El dni de la persona 3 es "+persona3.getDni());
+            System.out.println("El numero celular de la persona 3 es "+persona3.getNumeroCelular());
+            System.out.println("El direccion de la persona 3 es "+persona3.getDireccion());
+
+
+
+
+
     }
 }
--- ObjetoEnJava/src/Persona.java
+++ ObjetoEnJava/src/Persona.java
@@ -1,33 +1,81 @@
 public class Persona {
 
-   private  String nombre ;
-   private int edad;
-   private int dni;
-   private int numeroCelular;
-   private String direccion;
+    private String nombre;
+    private int edad;
+    private int dni;
+    private int numeroCelular;
+    private String direccion;
 
-
-   public void modificarNombre(String nombre){
-
-    this.nombre = nombre;
-
-   }
-
-   public String obtenerNombre(){
     
-    return this.nombre;
-   }
 
-   public void modificarEdad(int edad){
-    this.edad = edad;
-   }
-   public int obtenerEdad(){
-return this.edad;
-
-   }
+   
 
 
 
 
+    public Persona(String nombre , int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public Persona() {
+        
+    }
+
+
+    public Persona(String nombre , int edad , int dni , int numeroCelular , String direccion){
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dni = dni ;
+        this.numeroCelular = numeroCelular;
+        this.direccion = direccion;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public int getDni() {
+        return dni;
+    }
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+    public int getNumeroCelular() {
+        return numeroCelular;
+    }
+    public void setNumeroCelular(int numeroCelular) {
+        this.numeroCelular = numeroCelular;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+ 
 
 }
