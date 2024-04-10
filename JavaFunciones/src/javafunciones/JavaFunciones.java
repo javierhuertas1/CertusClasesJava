2ab32cc19cf85bdcbb1f24c3894a48518bc13a40
javierhuertas1 <javierhuertasmedrano385@gmail.com>
2024-04-09 19:55:07 -0500

    FuncionesEnJava

---
+++ JavaFunciones/src/javafunciones/JavaFunciones.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafunciones;
+
+/**
+ *
+ * @author mark42
+ */
+public class JavaFunciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        System.out.println("La suma del numero 5 y 2 es "+SumarNumero(5,2));
+        System.out.println("La resta del numero 4 y 2 es "+RestaEnteros(4, 2));
+        
+        
+        
+    }
+    
+    public static int SumarNumero(int valor1 , int valor2){
+
+        int respuesta;
+        respuesta = valor1 + valor2;
+        return respuesta;
+        
+    }
+    
+    
+    
+    
+    public static int RestaEnteros (int valor1 , int valor2){
+        int respuesta;
+        
+        respuesta = valor1 - valor2;
+        return respuesta;
+        
+    }
+    
+}
